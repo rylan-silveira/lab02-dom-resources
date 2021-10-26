67ff08dd8187e638c24b8316e4f61b3bdcc12851
rylan-silveira <54469690+rylan-silveira@users.noreply.github.com>
2021-10-26 09:26:12 +0400

    Update game.js

--- game.js
+++ game.js
@@ -18,6 +18,19 @@ function Bear() {
     this.htmlElement.style.top = this.y + "px";
     this.htmlElement.style.display = "block";
     };
+     this.fitBounds = function() {
+ let parent = this.htmlElement.parentElement;
+ let iw = this.htmlElement.offsetWidth;
+ let ih = this.htmlElement.offsetHeight;
+ let l = parent.offsetLeft;
+ let t = parent.offsetTop;
+ let w = parent.offsetWidth;
+ let h = parent.offsetHeight;
+ if (this.x < 0) this.x = 0;
+ if (this.x > w - iw) this.x = w - iw;
+ if (this.y < 0) this.y = 0;
+ if (this.y > h - ih) this.y = h - ih;
+ };
    }
 // Handle keyboad events 
 // to move the bear
